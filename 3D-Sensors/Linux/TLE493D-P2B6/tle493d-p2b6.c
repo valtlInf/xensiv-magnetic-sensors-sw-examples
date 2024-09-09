6ef7396feba79fd9dfb8d22a07ed9f875bbd7c2e
Pal Yashraj (CAE 1 B) <Yashraj.Pal@infineon.com>
2024-09-09 20:56:41 +0530

    TLE493D-X2B6 example code for Linux env

--- 3D-Sensors/Linux/TLE493D-P2B6/tle493d-p2b6.c
+++ 3D-Sensors/Linux/TLE493D-P2B6/tle493d-p2b6.c
@@ -3,36 +3,15 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
-#include <errno.h> // Error integer and strerror() function
+#include <errno.h> 
 #include <unistd.h>
 #include <linux/types.h>
 #include <linux/i2c-dev.h>
 #include <linux/i2c.h>
 
-int tli493d_write(int handle, unsigned char addr, unsigned char reg, unsigned char value);
-
-
-int tli493d_write(int handle, unsigned char addr, unsigned char reg, unsigned char value) {
-    unsigned char outbuf[2];
-    struct i2c_rdwr_ioctl_data packets;
-    struct i2c_msg messages[1];
-
-    messages[0].addr  = addr;
-    messages[0].flags = 0;
-    messages[0].len   = sizeof(outbuf);
-    messages[0].buf   = outbuf;
-
-    outbuf[0] = reg;
-    outbuf[1] = value;
-
-    packets.msgs  = messages;
-    packets.nmsgs = 1;
-    if(ioctl(handle, I2C_RDWR, &packets) < 0) {
-        return 0;
-    }
-
-    return 1;
-}
+#define B_sens  0.13  // Magnetic field resolution in mT/LSB12 
+#define T_sens  0.24  // Temperature resolution in K/LSB12
+int tle493d_write(int handle, unsigned char addr, unsigned char reg, unsigned char value);
 
 int main(){
 
@@ -56,18 +35,19 @@ int main(){
 
     
     // Configuring Mod1, for 1-byte read mode, clock stretching enabled, /INT disabled and Master Controlled mode, check FP bit odd parity between Mod2 & Mod1
-    result = tli493d_write(i2c_hd, 0x35, 0x11, 0x15);
+    result = tle493d_write(i2c_hd, 0x35, 0x11, 0x15);
     if (result == 0){
-        printf("Error in writing \n \r");
+        printf("Error in writing MOD1 register\n \r");
         return 0;
     }
 
     // Configuring Config, for Temp & Bz measuremnt enabled, ADC trigger on read after register 0x05, full range, no temperature compensation, check CP bit
-    result = tli493d_write(i2c_hd, 0x35, 0x10, 0x20);
+    result = tle493d_write(i2c_hd, 0x35, 0x10, 0x20);
     if (result == 0){
-        printf("Error in writing \n \r");
+        printf("Error in writing CONFIG register \n \r");
         return 0;
     }
+
     printf("Magnetic field values: \n \r");
     while(1){
         int bytes;
@@ -79,17 +59,39 @@ int main(){
             return 0;
         }
         
-        //12-bit data
-        __int16_t X = (__int16_t)((data[0]<<8) | (data[4] & 0xF0)) >> 4;
-        __int16_t Y = (__int16_t)((data[1]<<8) | ((data[4] & 0x0F) << 4)) >> 4;
-        __int16_t Z = (__int16_t)((data[2]<<8) | ((data[5] & 0x0F) << 4)) >> 4;
+        //concatenating the 12-bit data and multiplying by the resolution to get actual values
+        __double_t X = (__double_t)((((data[0]<<8) | (data[4] & 0xF0)) >> 4) * B_sens); 
+        __double_t Y = (__double_t)((((data[1]<<8) | ((data[4] & 0x0F) << 4)) >> 4) * B_sens);
+        __double_t Z = (__double_t)((((data[2]<<8) | ((data[5] & 0x0F) << 4)) >> 4) * B_sens);
         
-        __uint16_t T = (data[3] << 4) | (data[5] >> 4);
+        __double_t T = (((data[3] << 4) | (data[5] >> 4)) * T_sens);
 
         /*---------------------------------------------------------------------------------------------*/
         /*----------------------------------Enter your code here---------------------------------------*/
         /*---------------------------------------------------------------------------------------------*/
 
-        printf("Bx = %d\t By = %d\t Bz = %d \n\r", X, Y, Z);
+        printf("Bx = %lfmT\t By = %lfmT\t Bz = %lfmT\t T = %lfK \n\r", X, Y, Z, T);
     }
+}
+
+int tle493d_write(int handle, unsigned char addr, unsigned char reg, unsigned char value) {
+    unsigned char outbuf[2];
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[1];
+
+    messages[0].addr  = addr;
+    messages[0].flags = 0;
+    messages[0].len   = sizeof(outbuf);
+    messages[0].buf   = outbuf;
+
+    outbuf[0] = reg;
+    outbuf[1] = value;
+
+    packets.msgs  = messages;
+    packets.nmsgs = 1;
+    if(ioctl(handle, I2C_RDWR, &packets) < 0) {
+        return 0;
+    }
+
+    return 1;
 }
